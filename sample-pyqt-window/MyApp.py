bd2ba2f127d1b615f2f296c9f2627dc9b7384418
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2026-05-05 23:22:37 +0600

    A sample pyqt window

--- sample-pyqt-window/MyApp.py
+++ sample-pyqt-window/MyApp.py
@@ -1,12 +1,27 @@
-from PyQt6.QtWidgets import QApplication, QWidget
-
 import sys
 
+from PyQt6.QtCore import QSize, Qt
+
+from PyQt6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QPushButton,
+)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+
+        super().__init__()
+        self.setWindowTitle("Emran Test")
+        button = QPushButton("Click Now")
+
+        self.setCentralWidget(button)
+
+
 app = QApplication(sys.argv)
-window = QWidget()
-window.setWindowTitle("Test app")
 
-window.show()
-
+winddow = MainWindow()
+winddow.show()
 
 app.exec()
