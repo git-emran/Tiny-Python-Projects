c937f459f883a583d53eb7d73fbe0ce92b2acb3d
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2026-05-06 14:58:33 +0600

    Slots & Signals: UI data update

--- sample-pyqt-window/MyApp.py
+++ sample-pyqt-window/MyApp.py
@@ -13,20 +13,18 @@ class MainWindow(QMainWindow):
     def __init__(self):
 
         super().__init__()
-        self.button_was_checked = True
 
         self.setWindowTitle("Emran Test")
-
-        self.button = QPushButton("Click Now")
-        self.button.setCheckable(True)
-        self.button.released.connect(self.the_button_was_released)
-        self.button.setChecked(self.button_was_checked)
+        self.button = QPushButton("Click Me")
+        self.button.clicked.connect(self.the_button_was_clicked)
 
         self.setCentralWidget(self.button)
 
-    def the_button_was_released(self):
-        self.button_was_checked = self.button.isChecked()
-        print(self.button_was_checked)
+    def the_button_was_clicked(self):
+        self.button.setText("I have been clicked too many Times!")
+        self.button.setEnabled(False)
+
+        self.setWindowTitle("My OneShot app")
 
 
 app = QApplication(sys.argv)
