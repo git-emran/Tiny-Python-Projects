30f894a396bce7472e7c30b1086f234e3df76da3
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2026-05-09 13:26:46 +0600

    qss tester

--- sample-pyqt-window/qss-tester.py
+++ sample-pyqt-window/qss-tester.py
@@ -22,3 +22,48 @@ class MaindWindow(QMainWindow):
         super().__init__()
 
         self.setWindowTitle("QSS Tester")
+        self.editor = QPlainTextEdit()
+        self.editor.textChanged.connect(self.update_styles)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.editor)
+
+        cb = QCheckBox("Checkbox")
+        layout.addWidget(cb)
+
+        combo = QComboBox()
+        combo.setObjectName("thecombo")
+        combo.addItems(["First", "Second", "Third", "Fourth"])
+        layout.addWidget(combo)
+
+        sb = QSpinBox()
+        sb.setRange(0, 99999)
+        layout.addWidget(sb)
+
+        l = QLabel("This is a label")
+        layout.addWidget(l)
+
+        le = QLineEdit(l)
+        le.setObjectName("MyLineEdit")
+        layout.addWidget(le)
+
+        pb = QPushButton("Push me!")
+        layout.addWidget(pb)
+
+        self.container = QWidget()
+        self.container.setLayout(layout)
+
+        self.setCentralWidget(self.container)
+
+    def update_styles(self):
+        qss = self.editor.toPlainText()
+        self.setStyleSheet(qss)
+
+
+app = QApplication(sys.argv)
+app.setStyle("Fusion")
+
+w = MaindWindow()
+w.show()
+
+app.exec()
