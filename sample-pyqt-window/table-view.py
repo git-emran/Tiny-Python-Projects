74f796167a46f880f912079dbafdc965376e4bc0
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2026-05-12 19:41:32 +0600

    table View in QT

--- sample-pyqt-window/table-view.py
+++ sample-pyqt-window/table-view.py
@@ -1,10 +1,10 @@
 import sys
 
-from PyQt6 import QtCore, QtGui, QtWidgets
+from PyQt6 import QtCore, QtWidgets
 from PyQt6.QtCore import Qt
 
 
-class TableModel(QtCore.QAbstractListModel):
+class TableModel(QtCore.QAbstractTableModel):
     def __init__(self, data):
         super().__init__()
         self._data = data
@@ -12,3 +12,29 @@ class TableModel(QtCore.QAbstractListModel):
     def data(self, index, role):
         if role == Qt.ItemDataRole.DisplayRole:
             return self._data[index.row()][index.column()]
+
+    def rowCount(self, index):
+        return len(self._data)
+
+    def columnCount(self, index):
+        return len(self._data[0])
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.table = QtWidgets.QTableView()
+        data = [
+            [4, 1, 3, 3, 7],
+            [9, 3, 1, 1, 1],
+            [2, 1, 2, 5, 7],
+        ]
+        self.model = TableModel(data)
+        self.table.setModel(self.model)
+        self.setCentralWidget(self.table)
+
+
+app = QtWidgets.QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec()
